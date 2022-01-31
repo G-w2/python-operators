f8b5b0b3020d18809277b71778e0fd9ad719ca9f
gracewambui <72492434+G-w2@users.noreply.github.com>
2022-01-31 10:37:46 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,6 @@
+#assignment 
+a=6
+a+=a
+print(a)
+a*=a
+print(a)
---
+++ discount.py
@@ -0,0 +1,6 @@
+#discount
+purchase= int(input("enter your purchase"))
+if (purchase >= 1000):
+     print("discount",0.95* purchase)
+else:
+    print("no dicount",purchase)
